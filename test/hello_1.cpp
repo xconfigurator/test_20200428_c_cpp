ca5c971f621a4a66e81b415f3aa1f9ab6ff3357d
xconfigurator <xconfigurator@hotmail.com>
2020-04-28 18:10:41 +0800

    测试在ThinkPad X1 Tablet上提交

--- test/hello_1.cpp
+++ test/hello_1.cpp
@@ -13,7 +13,7 @@ using namespace std;
 */
 int main(int argc, char const *argv[])
 {
-  cout << "hello, world from openSUSE 15.1" << endl;
+  cout << "hello, world from openSUSE 15.1 and ThinkPad X1 Tablet" << endl;
   int a = 0;
   // std::system("pause");
   return 0;
