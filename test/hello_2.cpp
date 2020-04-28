0df4dab963f7bfe7aa489f39fb167a47dc87d94d
xconfigurator <xconfigurator@hotmail.com>
2020-04-28 16:47:54 +0800

    第一次提交

---
+++ test/hello_2.cpp
@@ -0,0 +1,11 @@
+// 一个工程，第二个main！！！
+#include <iostream>
+using namespace std;
+
+// shift + ctrl + p 添加task（就是使用g++编译的过程）
+
+int main(int argc, char const *argv[]) {
+  cout << "hello, world 2" << endl;
+  // std::system("pause");
+  return 0;
+}
---
+++ test/hello_3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// shift + ctrl + p 添加task（就是使用g++编译的过程）
+// shift + alt + f  格式化代码
+// shift + ctrl + d 仅编译
+/**
+* ctrl + p 快速导航文件
+* ctrl + shift + tab 循环浏览最后一组打开的文件
+* 
+* ctrl + shift + o 导航到文件中的特定符号
+* ctrl + g  导航到文件中的特定航
+*/
+int main(int argc, char const *argv[])
+{
+  cout << "hello, world 测试中文" << endl;
+  int a = 0;
+  // std::system("pause");
+  return 0;
+}
