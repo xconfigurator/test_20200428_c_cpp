5d40e808efaf0b95e05bea2edded4befc8f44ca3
xconfigurator <xconfigurator@hotmail.com>
2020-12-14 22:49:06 +0800

    00下的备考代码基本完成

---
+++ 02/00_practice/1.21.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char a = 'a', b = 'j';
+    float x;
+    x = (b - a) / ('F' - 'A'); // 9 / 5
+    printf("%d\n", (int) (3.14 * x));
+
+    // liuyang
+    printf("===================\n");
+    printf("%d\n", (int) (9 / 5));
+    printf("%d\n", (int) ((9 / 5) * 3.14));
+    return 0;
+}
