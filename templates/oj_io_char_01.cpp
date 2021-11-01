1a734a3e98dd01c6bbb5cfbbfacecc617d986e28
xconfigurator <xconfigurator@hotmail.com>
2021-11-01 10:28:10 +0800

    新版本的VSCode貌似原有配置不能用了，需要重新生成一下。

---
+++ templates/oj_io_char_01.cpp
@@ -0,0 +1,19 @@
+/*
+ref：天勤机考准备 04 字符输入输出
+时间：开始-
+
+输入一个字符：getchar()
+输出一个字符：putchar(字符变量或常量)
+
+e.g. 
+char ch = getchar();
+putchar(ch);
+*/
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    char ch = getchar();
+    putchar(ch);
+    return 0;
+}
