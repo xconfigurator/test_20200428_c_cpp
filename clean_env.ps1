f8d06c18a27abc5dca2a8d572612bb0abbad835d
xconfigurator <xconfigurator@hotmail.com>
2021-11-22 18:35:08 +0800

    增加清理项目

--- clean_env.ps1
+++ clean_env.ps1
@@ -1,13 +1,15 @@
 # @author liuyang
 # @since 2020/9/6
+# @update 2021/11/22 *.exe 改为 (*.exe, *.class)
 # Remove-Item ./02/00/*.exe -Recurse -Force
 
 $targetFolder = "."
-$files = Get-ChildItem $TargetFolder -Recurse -Include *.exe
+$targetFiles = ("*.exe", "*.class")
+$files = Get-ChildItem $targetFolder -Recurse -Include $targetFiles
 # Write-Output $files
 
 foreach ($file in $files) {
-  #Write-Output $file.FullName
+  Write-Output $file.FullName
   if ($file -is [System.IO.FileInfo]) {
     #Write-Output $file
     #Write-Output $file.FullName
