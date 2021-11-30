006d085f688823d3643907bc49a289c5df300617
xconfigurator <xconfigurator@163.com>
2021-11-30 11:22:31 +0800

    调整项目结构

---
+++ 02_C++/00/binary01.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file binary01.cpp
+ * @author liuyang
+ * @brief 位运算
+ * 
+ * 说明：
+ * 用于对整数类型（char, int, long等）变量中的某一位(bit)或者若干位进行操作。
+ * 比如：
+ * 1. 判断某一位是否是1。
+ * 2. 只改变其中某一位，而保持其他位都不变。
+ * 运算符：
+ * &    按位与（双目）
+ * |    按位或（双目）
+ * ^    按位异或（双目）
+ * ~    按位取反（单目）
+ * <<   左移（双目）
+ * >>   右移（双目）
+ * 
+ * @version 0.1
+ * @date 2021-11-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+
+
+int main(int argc, char const *argv[]) {
+    
+    return 0;
+}
