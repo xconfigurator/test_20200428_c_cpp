2b0d45af87cb5b600ee89243bc34fdb1823e0ed5
xconfigurator <xconfigurator@hotmail.com>
2021-12-01 19:58:01 +0800

    补充strcpy

--- 02_C++/00/str04strcpy.cpp
+++ 02_C++/00/str04strcpy.cpp
@@ -1,1 +1,31 @@
-// TODO 从那本C/C++参考书里面抄出来传说中那个完整的版本。
+/**
+ * @file str04strcpy.cpp
+ * @author liuyang copy from linrui
+ * @brief strcpy 来自林锐的《高质量程序设计指南 C++/C语言》
+ * @version 0.1
+ * @date 2021-12-01
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
+const int MAX_SIZE = 1000;
+
+char *Strcpy(char *strDest, const char *strSrc) {
+    assert((strDest != NULL) && (strSrc != NULL));
+    char *address = strDest;
+    while ((*strDest++ = *strSrc++) != '\0')
+        NULL;
+    return address;    
+}
+
+int main(int argc, char const *argv[]) {
+    char src[] = "hello, cpp";
+    char dest[MAX_SIZE];
+    Strcpy(dest, src);
+    cout << dest << endl;
+    return 0;
+}
