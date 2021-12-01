521b6a62fcd4bd7799a272e9a59c694ce3acaf06
xconfigurator <xconfigurator@hotmail.com>
2021-12-01 21:04:03 +0800

    修正

--- 02_C++/00/4.5.cpp
+++ 02_C++/00/4.5.cpp
@@ -14,12 +14,12 @@ p72
 using namespace std;
 
 class CArray {
-    int size; // 数组元素的个数
-    int *ptr; // 指向动态分配的数组
+    int size;                               // 数组元素的个数
+    int *ptr;                               // 指向动态分配的数组
 public:
-    CArray(int s = 0);  // s代表数组元素的个数
-    CArray(CArray & a); // 复制构造函数（深拷贝, 通过复制构造函数与重载=运算符配合完成）
-    virtual ~CArray();  // 清理！
+    CArray(int s = 0);                      // s代表数组元素的个数
+    CArray(CArray & a);                     // 复制构造函数（深拷贝, 通过复制构造函数与重载=运算符配合完成）
+    virtual ~CArray();                      // 清理！
     friend ostream & operator << (ostream & os, const CArray & a); // toString()
     CArray & operator = (const CArray & a); // 对象复制（深拷贝, 通过复制构造函数与重载=运算符配合完成）
 
@@ -28,6 +28,7 @@ public:
         return size;
     }
     // 注意返回值的类型是 int &
+    // 非引用类型的函数返回值不能做左值
     // 实现功能：a[i] = 4;和 n = a[i];
     int & operator[](int i) {
         return ptr[i];
@@ -36,6 +37,7 @@ public:
     void push_back(int v);  
 };
 
+// 12:26
 CArray::CArray(int s):size(s) {
     if (0 == s) {
         ptr = NULL;
@@ -44,8 +46,9 @@ CArray::CArray(int s):size(s) {
     }
 }
 
+// 13:12 
 CArray::CArray(CArray & a) {// 深拷贝
-    // 对方空
+    // 对方空（两个等价写法）
     // if (!a.ptr) {
     if (NULL == a.ptr) {
         ptr = NULL;
@@ -58,6 +61,7 @@ CArray::CArray(CArray & a) {// 深拷贝
     size = a.size;
 }
 
+// 16:02
 CArray::~CArray() {
     if (ptr != NULL) {
         delete[] ptr;
@@ -74,14 +78,15 @@ ostream & operator << (ostream & os, const CArray & a) {
     return os;
 }
 
+// 16:17
 CArray & CArray::operator = (const CArray & a) {// 深拷贝
     // a = a
-    if (ptr == a.ptr) {
+    if (ptr == a.ptr) {// 防止 a = a这样的赋值操作导致错误。
         return *this;
     }
     // 对方空
     if (NULL == a.ptr) {
-        if (ptr != NULL) {
+        if (ptr != NULL) {// 被赋值的对象之前有内容
             delete[] ptr;
         }
         ptr = NULL;
@@ -89,7 +94,7 @@ CArray & CArray::operator = (const CArray & a) {// 深拷贝
         return *this;
     }
     // 对方不空
-    // 对方长度超过自己,则需要申请新的空间。
+    // 对方长度超过自己,则需要申请新的空间。（原有空间足够大，就不用分配新的空间）
     if (size < a.size) {
         if (ptr != NULL) {
             delete[] ptr;
@@ -101,16 +106,22 @@ CArray & CArray::operator = (const CArray & a) {// 深拷贝
     return *this;
 }
 
+// 19:29
 void CArray::push_back(int v) {
     // 一个效率低但是好写的版本
     // 描述：每次添加元素都重新申请空间并拷贝，然后销毁原有空间。
-    int * tmpPtr = new int[size + 1];
-    memcpy(tmpPtr, ptr, sizeof(int) * size);
-    delete[] ptr;
-    ptr = tmpPtr;
+    if (ptr) {
+        int * tmpPtr = new int[size + 1];
+        memcpy(tmpPtr, ptr, sizeof(int) * size);
+        delete[] ptr;
+        ptr = tmpPtr;
+    } else {
+        ptr = new int[1];
+    }
     ptr[size ++] = v; // 加入新元素
 }
 
+// 视频一开始就介绍了这个类的外部接口
 int main(int argc, char const *argv[]) {
     CArray a;
     for (int i = 0; i < 5; ++i) {
@@ -121,12 +132,16 @@ int main(int argc, char const *argv[]) {
     // 测试 = 
     a2 = a;
     cout << a2 << endl;
-    a2 = a3;    // 清空a2
+    a2 = a3;            // 清空a2
     cout << a2 << endl;
 
     // 测试[]
     a[3] = 100;
     cout << a << endl;
+
+    // 测试复制构造函数
+    CArray a4(a);        
+    cout << "a4 = " << a4 << endl;
 
     return 0;
 }
