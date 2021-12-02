157c7d2497782eaa34454c142ee4c0bea1bab5ab
xconfigurator <xconfigurator@163.com>
2021-12-02 13:15:48 +0800

    队列的例子

--- 04_OJ/oj_leetcode_70/LC933.cpp
+++ 04_OJ/oj_leetcode_70/LC933.cpp
@@ -1,13 +1,11 @@
-/*
-题目：https://leetcode-cn.com/problems/number-of-recent-calls/
-讲解：TODO
-*/
-
-
 /**
  * @file LC933.cpp
  * @author liuyang
  * @brief 最近的请求次数 The Number of Calls
+ * 
+ * 题目：https://leetcode-cn.com/problems/number-of-recent-calls/
+ * OJ状态：通过
+ * 
  * @version 0.1
  * @date 2021-12-02
  * 
@@ -18,7 +16,29 @@
 #include <queue>
 using namespace std;
 
-// TODO
+class RecentCounter {
+public:
+    RecentCounter() {
+
+    }
+    
+    /*
+    两组：
+    add/remove/element
+    offer/poll/peek
+    联想C++ STL:
+    push/pop/ front back
+    */
+    int ping(int t) {
+        queue.push(t);
+        while (!queue.empty() && (t - queue.front() > 3000)) {
+            queue.pop();
+        }
+        return queue.size();
+    }
+private:
+    queue<int> queue;
+};
 
 int main() {
 
--- 04_OJ/oj_leetcode_70/LC933.java
+++ 04_OJ/oj_leetcode_70/LC933.java
@@ -1,8 +1,8 @@
 /*
 @auther liuyang
 @since 2021/12/2
-参考：https://www.bilibili.com/read/cv8623235?from=articleDetail
-OJ状态：TODO
+ref：https://www.bilibili.com/read/cv8623235?from=articleDetail
+OJ：accept
 */
 import java.util.LinkedList;
 import java.util.Queue;
@@ -12,19 +12,12 @@ class LC933 {
     Queue<Integer> queue;
 
     public LC933() {
-        queue = new LinkedList<Integer>();
+        this.queue = new LinkedList<Integer>();
     }
 
-    /*
-    两组：
-    add/remove/element
-    offer/poll/peek
-    联想C++ STL:
-    push/pop/ front back
-    */
     public int ping(int i) {
         queue.add(i);
-        while (!queue.isEmpty() && (t - queue.element() > 3000) {
+        while (!queue.isEmpty() && (i - queue.element() > 3000)) {
             queue.remove();
         }
         return queue.size();
